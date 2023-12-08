15cf7f7d2356e58ab7380c07594e63c5bddd14e9
胡子银 <huziyin@huziyindeMacBook-Air.local>
2023-12-08 11:20:18 +0800

    feat: init

---
+++ note.md
@@ -0,0 +1,10 @@
+"cookie-parser": "~1.4.4", // 解析cookie数据
+"debug": "~2.6.9", // 
+"express": "~4.16.1",
+"http-errors": "~1.6.3",
+"jade": "~1.11.0",
+"morgan": "~1.9.1"  处理日志
+
+cors // 处理跨域请求
+compression // 压缩http请求
+nodemon // 热更新
---
+++ routes/todo.js
@@ -0,0 +1,29 @@
+
+
+var express = require('express');
+var router = express.Router();
+
+
+// 查看
+router.get('/',(req,res)=>{
+    res.send('respond with a todo');
+})
+
+// 增加
+router.post('',(req,res)=>{
+    // todo
+})
+
+// 修改
+router.post('',(req,res)=>{
+    // todo
+})
+
+// 删除
+router.post('',(req,res)=>{
+    // todo
+})
+
+
+
+module.exports = router;
